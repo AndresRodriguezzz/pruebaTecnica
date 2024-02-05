d69a7d2095844fba8c2714fb93423d517accc9d5
AndresRodriguezzz <andres00rb@gmail.com>
2024-02-05 16:10:55 -0500

    modified:   src/App.jsx 	modified:   src/components/form.jsx 	modified:   src/components/layout.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
   <>
   <Layout>
-    <MyForm/>
+    <MyForm isAuthenticated={false}/>
   </Layout>
   </>
   );
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,7 +1,22 @@
 
 import { useState } from "react";
 
-export function MyForm() {
+export function MyForm({initialIsAuthenticated}) {
+
+  const [isAuthenticated, setAuthentificated]=useState(initialIsAuthenticated)
+
+  const authClick = () => {
+    if(values.password != '' && values.email != ''){
+      setAuthentificated(!isAuthenticated);
+    }else{
+      alert('Se encuentras vacios los campos');
+    }
+  }
+
+  const signOut = () => {
+    setAuthentificated(!isAuthenticated);
+  }
+
   const [values, setValues] = useState({
     email: "",
     password: "",
@@ -23,45 +38,62 @@ export function MyForm() {
 
     setValues(newValues);
   }
+  
+  if(isAuthenticated === true){
+    return (
+      <div className="grid grid-cols-1 content-center">
+        <form className="mx-auto max-w-md space-y-3" onSubmit={handleSubmit}>
+          <div className="">
+            <label className="block text-sm font-bold text-gray-700" htmlFor="email">
+              Email:
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              value={values.email}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-indigo-500"
+            />
+          </div>
+          
+          <div className="mb-4">
+            <label className="block text-sm font-bold text-gray-700" htmlFor="password">
+              Contraseña:
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              value={values.password}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-indigo-500"
+            />
+          </div>
 
-  return (
-    <div className="grid grid-cols-1 content-center">
-      <form className="mx-auto max-w-md space-y-4" onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label className="block text-sm font-bold text-gray-700" htmlFor="email">
-            Email:
-          </label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            value={values.email}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-indigo-500"
-          />
-        </div>
-        
-        <div className="mb-4">
-          <label className="block text-sm font-bold text-gray-700" htmlFor="password">
-            Contraseña:
-          </label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            value={values.password}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-indigo-500"
-          />
-        </div>
+          <div className="text-center">
+            <button 
+            className="bg-red-500 text-white rounded-lg px-4 py-2" 
+            type="submit"
+            onClick={authClick}>
+              Ingresar
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }else{
+    return(
+      <>
+        <h1>Bienvenido</h1>
+        <button
+         onClick={signOut}
+         className="bg-red-500 text-white rounded-lg px-4 py-2">
+          Salir
+        </button>
+      </>
+    )
+  }
 
-        <div className="text-center">
-          <button className="bg-red-500 text-white rounded-lg px-4 py-2" type="submit">
-            Ingresar
-          </button>
-        </div>
-      </form>
-    </div>
-  );
 }
 
